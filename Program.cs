8e6b4f8de10ce102da099dee2d2f0177dc3ccf9b
Aishwarya C <chan.aishwarya@gmail.com>
2024-02-09 17:07:47 +0530

    [AISHWARYA] Add: Print 123 45 6 in a inverse Right Angled Triangle

--- Program.cs
+++ Program.cs
@@ -5,15 +5,18 @@ public class FileStreamExample
 {
     public static void Main(string[] args)
     {
-        for (int i = 1; i <= 3; i++)
+        int val = 1;
+        for (int i = 0 ; i <=3 ; i++)
         {
 
-            for (int j = 1; j <= i; j++)
+            for (int j =0 ; j<3-i ; j++)
             { 
-           Console.Write(j); 
+                Console.Write(val);
+                val++;
             }
             Console.WriteLine();
         }
+
         Console.ReadLine();
     }
 
