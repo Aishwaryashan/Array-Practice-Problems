2662c0f4cf9970f462bf6a960ce0ece291a2a2c5
Aishwarya C <chan.aishwarya@gmail.com>
2024-02-09 15:50:29 +0530

    [AISHWARYA] Add: Reverse Right Angled Triangle

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ public class FileStreamExample
     {
         for(int i=1; i<=4; i++)
         {
-            for (int j = 1; j <= i; j++)
+            for (int j = 4; j>=i; j--)
             { 
                 Console.Write("*");
             }
