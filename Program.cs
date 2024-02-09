def2e6807ba299b12a007a5ee7461d4d0f49aca6
Aishwarya C <chan.aishwarya@gmail.com>
2024-02-09 14:42:04 +0530

    [AISHWARYA] Add: Pattern problem

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+public class FileStreamExample
+{
+    public static void Main(string[] args)
+    {
+        /*
+         * int[] arr = { 3, 4, 5, 6, 7, 8 };
+         int sum = 0;
+         for (int i= 0; i<6; i++)
+         {
+             sum += arr[i]; 
+
+         }
+         Console.WriteLine(sum);
+         Console.ReadLine();
+        */
+
+
+        /* factorial of 10
+         * int mul = 1;
+
+        for (int i=10; i>= 1; i--) 
+            {
+            mul *= i;
+            }
+        Console.WriteLine(mul);
+        Console.ReadLine();*/
+
+
+
+
+        /*for(int i=1; i<=3; i++)
+        {
+            Console.WriteLine($"{ i}{i}");
+        }
+        for (int i=2; i>=1; i--)
+        {
+            Console.WriteLine($"{ i}{i}");
+        }
+        Console.ReadLine();*/
+
+        /* for (int i =0; i<=3; i++)
+         {
+             for (int j = 0; j <= 4; j++)
+             {
+
+                 Console.WriteLine("j");
+             }
+                 Console.WriteLine("i");
+         }
+             Console.ReadLine();*/
+
+
+
+
+
+        /* prime number (mistake)
+         int = x;
+         x = Convert.ToInt32(Console.ReadLine());
+
+         for (int i = 1; i <= x; i++)
+         {
+             if(int x % 1 = 0)
+                 {
+
+             Console.WriteLine("PRIMENUMBER");
+                }
+             else
+         {
+             Console.WriteLine("notaprime");
+         }
+     }
+             Console.ReadLine();*/
+
+
+
+
+        /*even odd
+        int x = 14;
+        int y = x % 2;
+        if(y==0 )
+                {
+            Console.WriteLine("even");
+        }
+        else 
+        {
+            Console.WriteLine("no");
+                }
+        Console.ReadLine()
+       ;*/
+
+
+
+        int x, y;
+        for ( int i = 0; i <=3; ++i )
+        {
+            for (int j = 0; j <= 3; j++)
+            {
+                Console.Write("9");
+            }
+                        Console.WriteLine("*");
+        }
+
+        
+        Console.ReadLine();
+    }
+    
+         
+}
