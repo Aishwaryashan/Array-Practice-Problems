78a59892531d93d75e9e64542528f54911859f3f
Aishwarya C <chan.aishwarya@gmail.com>
2024-02-09 16:49:50 +0530

    [AISHWARYA] Add: Print 1  12  123 in a Triangle format

--- Program.cs
+++ Program.cs
@@ -5,13 +5,14 @@ public class FileStreamExample
 {
     public static void Main(string[] args)
     {
-        for(int i=1; i<=4; i++)
+        for (int i = 1; i <= 3; i++)
         {
-            for (int j = 4; j>=i; j--)
+
+            for (int j = 1; j <= i; j++)
             { 
-                Console.Write("*");
+           Console.Write(j); 
             }
-        Console.WriteLine();
+            Console.WriteLine();
         }
         Console.ReadLine();
     }
