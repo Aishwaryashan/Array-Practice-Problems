e9c6d2a1eae9011eb78755c65f0a522c018c0667
Aishwarya C <chan.aishwarya@gmail.com>
2024-02-10 11:49:47 +0530

    [AISHWARYA] Add : Printing the whole 5 table in the output

--- Program.cs
+++ Program.cs
@@ -5,17 +5,15 @@ public class FileStreamExample
 {
     public static void Main(string[] args)
     {
-
-        int[] arr = { 4, 1, 3, 5, 8, 9 };
-        int max=arr[0];
-        for (int i = 0; i <arr.Length;i++)
+        
+          
+        for (int i=1; i<=10; i++)
         {
-            if (arr[i]>max)
-            {
-                max=arr[i];
-            }
+
+            Console.WriteLine($" 5* {i} = { 5 * i}");
+            
         }
-        Console.WriteLine(max);
+      
         
         Console.ReadLine();
     }
