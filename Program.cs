f3eb98032ba02b40f4572cbc4797caa9a6b331d7
Aishwarya C <chan.aishwarya@gmail.com>
2024-02-10 12:07:03 +0530

    [AISHWARYA] Add : Sum of all the numbers in the array

--- Program.cs
+++ Program.cs
@@ -4,17 +4,15 @@ using System.IO;
 public class FileStreamExample
 {
     public static void Main(string[] args)
-    {
-        
-          
-        for (int i=1; i<=10; i++)
+    { 
+        int sum = 0;
+        int[] arr = { 1, 2, 3, 4, 5 };
+         for ( int i = 0; i <=4 ; i++ )
         {
-
-            Console.WriteLine($" 5* {i} = { 5 * i}");
-            
+             sum += arr[i];     
         }
-      
-        
+        Console.WriteLine(sum);
+
         Console.ReadLine();
     }
     }
